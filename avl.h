2fe92322d04646f5b1372c88a3187c2a5d8fd7ae
Yonatan <yonatan@lischinsky.com>
2021-11-29 19:04:37 +0200

    Added gilgolgs (not checked yet)

--- avl.h
+++ avl.h
@@ -1,10 +1,25 @@
 #ifndef AVL.H
 #define AVL.H
 #include <memory>
+#include "library1.h"
 
 
 namespace ds
 {
+    typedef enum
+    {
+        LEFT = 0,
+        RIGHT
+    } INSERT_DIRECTION ;
+
+    typedef enum
+    {
+        LL = 0,
+        LR,
+        RL,
+        RR
+    } GILGOL_TYPE ;
+
     template<class T>
     class Avl
     {
@@ -12,7 +27,7 @@ namespace ds
         Avl(); //constructor
         ~Avl(); //destructor
 
-        void insert(T data, int key);
+        StatusType Avl<T>::insert(T data, int key);
 
         //Avl(const Avl&); //copy constructor
 
@@ -20,12 +35,14 @@ namespace ds
         class Node;
         std::shared_ptr<Node> root;
 
-        void DeleteAllNodesPostOrder(std::shared_ptr< Avl<T>::Node > r);
+        //private helper functions:
+        std::shared_ptr<Avl<T>::Node> binarySearch(int insert_key, std::shared_ptr<Avl<T>::Node> sub_root, INSERT_DIRECTION* dir);
+        void AfterInsertCheckTree(std::shared_ptr<Avl<T>::Node> v);
     };
 
 
 
-#pragma region  Node
+#pragma region Node
 
 
     /* *************************** Node Implementation Below *****************************/
@@ -33,7 +50,6 @@ namespace ds
     class Avl<T>::Node
     {
     public:
-        //template<class K>
         Node(T& data, int key); //Constructor
         Node(const Node& node); //copy constructor
         ~Node(); //destructor
@@ -41,16 +57,23 @@ namespace ds
         T data;
         //template<class K>
         int key;
+        int balance_factor;
+        int height;
 
         std::shared_ptr<Node> left;
         std::shared_ptr<Node> right;
         std::shared_ptr<Node> father;
+
+        void UpdateBF();
+        void UpdateHeight();
+
+        
     };
 
     /* Node Constructor */
     template<class T>
     //template<class K>
-    Avl<T>::Node::Node(T& data, int key) : data(data), key(key), left(nullptr), right(nullptr), father(nullptr)
+    Avl<T>::Node::Node(T& data, int key) : data(data), key(key), left(nullptr), right(nullptr), father(nullptr), balance_factor(0), height(0)
     {
     }
 
@@ -65,9 +88,33 @@ namespace ds
     Avl<T>::Node::~Node()
     {
     }
+
+    template<class T>
+    void Avl<T>::Node::UpdateBF()
+    {
+        int hL, hR;
+
+        if(this->left == nullptr)
+            hL = -1;
+        else
+            hL = this->left->height;
+
+        if(this->right == nullptr)
+            hR = -1;
+        else
+            hR = this->right->height;
+            
+        this->balance_factor = hL - hR;
+    }
+
+    template<class T>
+    void Avl<T>::Node::UpdateHeight()
+    {
+        this->height = max(this->left->height, this->right->height) + 1;
+    }
 #pragma endregion
     
-    
+#pragma region Avl Tree
 /* *************************** Avl Implementation Below *****************************/
 
     /* AVL Constructor */
@@ -80,47 +127,250 @@ namespace ds
     template<class T>
     Avl<T>::~Avl()
     {
-        DeleteAllNodesPostOrder(this->root);
     }
 
     template<class T>
-    void Avl<T>::insert(T data, int key)
+    StatusType Avl<T>::insert(T data, int key)
     {
-
-        std::shared_ptr< Avl<T>::Node > nodeToInsert ( new Node(data, key));
-
-
-        std::shared_ptr <Avl<T>::Node> ptr = this->root;
-
-        if (ptr != nullptr)
+        if(this->root == nullptr)
         {
-            while (ptr->left != nullptr)
+            try
             {
-                ptr = ptr->left; //The visual studio didnt auto complete here... check if works???
+                std::shared_ptr< Avl<T>::Node > nodeToInsert ( new Node(data, key));
+                this->root = nodeToInsert;
+                return StatusType.SUCCESS;
             }
-            ptr->left = nodeToInsert;
+            catch(const std::exception& e)
+            {
+                return StatusType.ALLOCATION_ERROR;
+            }
+        }
+        
+        //Check if already exists:
+        INSERT_DIRECTION dir;
+        std::shared_ptr<Avl<T>::Node> father = binarySearch(key, this->root, &dir);
+        if(father == nullptr) //Key already exists !
+        {
+            return StatusType.FAILURE;
+        }
+
+        try
+        {
+            std::shared_ptr< Avl<T>::Node > nodeToInsert ( new Node(data, key));
+            if(dir == INSERT_DIRECTION.LEFT)
+            {
+                father->left = nodeToInsert;
+            }
+            else
+            {
+                father->right = nodeToInsert;
+            }
+
+            //Make gilgols:
+
+        }
+        catch(const std::exception& e)
+        {
+            return StatusType.ALLOCATION_ERROR;
+        }
+    }
+
+    template<class T>
+    void Avl<T>::AfterInsertCheckTree(std::shared_ptr<Avl<T>::Node> v)
+    {
+        while(v != this->root)
+        {
+            std::shared_ptr<Avl<T>::Node> p = v->father;
+            if(p->height >= v->height + 1)
+                return;
+            p->height = v->height + 1;
+
+            p->UpdateBF();
+
+
+            switch (p->balance_factor)
+            {
+            case 2:
+                if(p->left->balance_factor >= 0)
+                {
+                    Gilgol(p, GILGOL_TYPE.LL);
+                }
+                else if(p->left->balance_factor == -1) //maybe else here ?...
+                {
+                    Gilgol(p, GILGOL_TYPE.LR);
+                }
+                return;
+
+            case -2:
+                if(p->right->balance_factor >= 0)
+                {
+                    Gilgol(p, GILGOL_TYPE.RR);
+                }
+                else if(p->right->balance_factor == -1) //maybe else here ?...
+                {
+                    Gilgol(p, GILGOL_TYPE.RL);
+                }
+                return;
+
+            default: //BF is ok
+                v = p;
+                break;
+            }
+        }
+    }
+    
+    template<class T>
+    void Gilgol(std::shared_ptr<Avl<T>::Node> r, GILGOL_TYPE type)
+    {
+        switch (type)
+        {
+        case GILGOL_TYPE.LL:
+                std::shared_ptr<Avl<T>::Node> tempA = r->left;
+
+                r->left = tempA->right;
+                tempA->right->father = r;
+                tempA->right = r;
+                tempA->father = r->father;
+                
+                if(r->father->right == r)
+                {
+                    r->father->right = tempA;
+                }
+                else
+                {
+                    r->father->left = tempA;
+                }
+                r->father = tempA;
+
+                            
+                r->UpdateHeight();
+                tempA->UpdateHeight();
+            break;
+        
+        case GILGOL_TYPE.RR:
+                std::shared_ptr<Avl<T>::Node> tempA = r->right;
+
+                r->right = tempA->left;
+                tempA->left->father = r;
+                tempA->left = r;
+                tempA->father = r->father;
+                
+                if(r->father->right == r)
+                {
+                    r->father->right = tempA;
+                }
+                else
+                {
+                    r->father->left = tempA;
+                }
+                r->father = tempA;
+
+                r->UpdateHeight();
+                tempA->UpdateHeight();
+            break;
+
+        case GILGOL_TYPE.LR:
+            std::shared_ptr<Avl<T>::Node> tempA = r->left;
+            std::shared_ptr<Avl<T>::Node> tempB = r->left->right;
+            r->left = tempB->right;
+            tempB->right->father = r;
+
+            tempA->right = tempB->left;
+            tempB->left->father = tempA;
+
+            tempB->left = tempA;
+            tempA->father = tempB;
+
+            tempB->right = r;
+
+            if(r->father->right == r)
+            {
+                r->father->right = tempB;
+            }
+            else
+            {
+                r->father->left = tempB;
+            }
+            r->father = tempB; //Keep this under the if/else
+
+            tempA->UpdateHeight();
+            r->UpdateHeight();
+            tempB->UpdateHeight();
+
+            break;
+
+        case GILGOL_TYPE.RL:
+            std::shared_ptr<Avl<T>::Node> tempA = r->right;
+            std::shared_ptr<Avl<T>::Node> tempB = r->right->left;
+            r->right = tempB->left;
+            tempB->left->father = r;
+
+            tempA->left = tempB->right;
+            tempB->right->father = tempA;
+
+            tempB->right = tempA;
+            tempA->father = tempB;
+
+            tempB->left = r;
+
+            if(r->father->right == r)
+            {
+                r->father->right = tempB;
+            }
+            else
+            {
+                r->father->left = tempB;
+            }
+            r->father = tempB; //Keep this under the if/else
+
+            tempA->UpdateHeight();
+            r->UpdateHeight();
+            tempB->UpdateHeight();
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    template<class T>
+    std::shared_ptr<Avl<T>::Node> Avl<T>::binarySearch(int insert_key, std::shared_ptr<Avl<T>::Node> sub_root, INSERT_DIRECTION* dir)
+    {
+        if(dir == nullptr)
+        {
+            //should'nt get in here...
+            return nullptr;
+        }
+
+        if(sub_root.get() == nullptr)
+        {
+            return nullptr;
+        }
+
+        if(insert_key < sub_root->key)
+        {
+            if(sub_root->left == nullptr)
+            {
+                *dir = INSERT_DIRECTION.LEFT;
+                return sub_root;
+            }
+            return binarySearch(insert_key, sub_root->left, dir);
+        }
+        else if( insert_key == sub_root->key)
+        {
+            *dir = INSERT_DIRECTION.FAIL;
+            return nullptr;
         }
         else
         {
-            this->root = nodeToInsert;
+            if(sub_root->right == nullptr)
+            {
+                *dir = INSERT_DIRECTION.RIGHT;
+                return sub_root;
+            }
+            return binarySearch(insert_key, sub_root->right, dir);
         }
-
     }
-
-    template<class T>
-    void Avl<T>::DeleteAllNodesPostOrder(std::shared_ptr< Avl<T>::Node > r)
-    {
-        if (r == nullptr)
-        {
-            return;
-        }
-
-        DeleteAllNodesPostOrder(r->left);
-        DeleteAllNodesPostOrder(r->right);
-
-        //delete();
-    }
+#pragma endregion
 }
-
-
 #endif
